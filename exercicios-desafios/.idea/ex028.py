93dc42694961bb6ed90738451eaba6e4ad2ae115
André Moraes <124800489+andre09moraes@users.noreply.github.com>
2023-04-11 18:26:29 -0300

    update repositorio python

--- exercicios-desafios/.idea/ex028.py
+++
@@ -1,7 +0,0 @@
-import random
-num = random.randint(0, 5)
-tent = int(input('Digite um número de 0 a 5: '))
-if tent == num:
-    print('Parabéns, você acertou!')
-else:
-    print('Você errou, o número era {}'.format(num))
